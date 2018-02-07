b9fd999d4e442775f8061087ce68444cc5c63d07
Jared <jradtkey@gmail.com>
2018-02-06 17:17:56 -0800

    added pause

--- static/scripts/final.js
+++ static/scripts/final.js
@@ -1,9 +1,24 @@
 $(document).ready(function() {
 
+  var beat0 = document.getElementById('beat0');
+  var beat1 = document.getElementById('beat1');
+  var beat2 = document.getElementById('beat2');
+  var beat3 = document.getElementById('beat3');
+  beat0.currentTime = 0;
+  beat1.currentTime = 0;
+  beat2.currentTime = 0;
+  beat3.currentTime = 0;
+  beat0.volume = .5;
+  beat1.volume = .5;
+  beat2.volume = .5;
+  beat3.volume = .5;
   var size_bass = document.getElementById('bass')
   var size_drums = document.getElementById('drums')
   var size_sample = document.getElementById('sample')
   var size_keyboard = document.getElementById('keyboard')
+  var play = false;
+
+  //styling for shorter instruments
   $('#drums').css('margin-top', (size_bass.clientHeight-size_drums.clientHeight)+'px');
   $('#sample').css('margin-top', (size_bass.clientHeight-size_sample.clientHeight)+'px');
   $('#keyboard').css('margin-top', (size_bass.clientHeight-size_keyboard.clientHeight)+'px');
@@ -16,22 +31,25 @@ $(document).ready(function() {
 
 
   $('#button').click(function() {
-    var beat0 = document.getElementById('beat0');
-    var beat1 = document.getElementById('beat1');
-    var beat2 = document.getElementById('beat2');
-    var beat3 = document.getElementById('beat3');
-    beat0.currentTime = 0;
-    beat1.currentTime = 0;
-    beat2.currentTime = 0;
-    beat3.currentTime = 0;
-    beat0.volume = .5;
-    beat1.volume = .5;
-    beat2.volume = .5;
-    beat3.volume = .5;
-    beat0.play();
-    beat1.play();
-    beat2.play();
-    beat3.play();
+    console.log(play);
+    if (!play) {
+      $('#toggle').html('<i class="fas fa-pause-circle"></i>');
+      play = true;
+      beat0.play();
+      beat1.play();
+      beat2.play();
+      beat3.play();
+    }
+    else {
+      $('#toggle').html('<i class="fas fa-play-circle"></i>');
+      play = false;
+      beat0.pause();
+      beat1.pause();
+      beat2.pause();
+      beat3.pause();
+    }
+
+    console.log(play);
 
   })
 
@@ -42,6 +60,8 @@ $(document).ready(function() {
           $("#volume0").html(ui.value)
         }
   });
+
+  $("#button").hover(console.log("hovered"));
 
   $("#slider1").slider({
       value: 50,
